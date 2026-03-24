53f819fff0fb233e3b0e1b17b4955f565beb0d24
prasanth_s <prasanthsakthi122000@gmail.com>
2026-03-24 15:00:23 +0530

    JS tasks completed

--- JavaScript/Day_1/js/task1.js
+++ JavaScript/Day_1/js/task1.js
@@ -1,35 +1,155 @@
 
 // Var
-var userName;
+var userName="Prasanth";
 console.log(userName);
 
-var userId;
+var userId=45;
 console.log(userId);
 
-var userAge;
+var userAge=26;
 console.log(userAge);
 
-var userAddress;
+var userAddress="Chennai";
 console.log(userAddress);
 
-var userContact;
+var userContact=4545454545;
 console.log(userContact);
 
-var userMarks;
+var userMarks=85;
 console.log(userMarks);
 
-var telephoneNumber;
+var telephoneNumber=4545454545;
 console.log(telephoneNumber);
 
-var vehicleName;
+var vehicleName="Mercedes Benz";
 console.log(vehicleName);
 
-var vehicleType;
+var vehicleType="Car";
 console.log(vehicleType);
 
-var alternateName;
+var alternateName="Loki";
 console.log(alternateName);
 
 // let
+
+let FirstName = "Prasanth";
+console.log(FirstName);
+
+let LastName = "S";
+console.log(LastName);
+
+let UserAccount = "prasanth.s";
+console.log(UserAccount);
+
+let LoginPage = "login";
+console.log(LoginPage);
+
+let OrderId = 10;
+console.log(OrderId);
+
+let ProductId = 100;
+console.log(ProductId);
+
+let MobileNumber = 4545454545;
+console.log(MobileNumber);
+
+let EmailId = "prasanthsakthi122000@gmail.com";
+console.log(EmailId);
+
+let HomePage = "Google";
+console.log(HomePage);
+
+let CardNumber = 123456789;
+console.log(CardNumber);
+
+// const
+
+const employee_name="Prasanth";
+console.log(employee_name);
+
+const employee_id=45;
+console.log(employee_id);
+
+const employee_contact=4545454545;
+console.log(employee_contact);
+
+const employee_email="qqq@gmail.com";
+console.log(employee_email);
+
+const employee_address="Chennai";
+console.log(employee_address);
+
+const course_name="Full Stack";
+console.log(course_name);
+
+const laptop_brand="Asus";
+console.log(laptop_brand);
+
+const screen_size=16;
+console.log(screen_size);
+
+const battery_capacity=80;
+console.log(battery_capacity);
+
+const internet_speed="5G";
+console.log(internet_speed);
+
+const languages_known=2;
+console.log(languages_known);
+
+const class_name="Technical";
+console.log(class_name);
+
+const classroom_number=15;
+console.log(classroom_number);
+
+const project_name="Roadside Assistance";
+console.log(project_name);
+
+const project_id=3;
+console.log(project_id);
+
+const employee_experience=5;
+console.log(employee_experience);
+
+const employee_salary=50000;
+console.log(employee_salary);
+
+const programming_language="JavaScript";
+console.log(programming_language);
+
+const favourite_game="God of War";
+console.log(favourite_game);
+
+const favourite_movie="Avengers Endgame";
+console.log(favourite_movie);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
