6db1f41c2985918b961a94c7489b826a34d26460
prasanth_s <prasanthsakthi122000@gmail.com>
2026-03-24 09:16:03 +0530

    Js update

---
+++ JavaScript/Day_1/js/task1.js
@@ -0,0 +1,35 @@
+
+// Var
+var userName;
+console.log(userName);
+
+var userId;
+console.log(userId);
+
+var userAge;
+console.log(userAge);
+
+var userAddress;
+console.log(userAddress);
+
+var userContact;
+console.log(userContact);
+
+var userMarks;
+console.log(userMarks);
+
+var telephoneNumber;
+console.log(telephoneNumber);
+
+var vehicleName;
+console.log(vehicleName);
+
+var vehicleType;
+console.log(vehicleType);
+
+var alternateName;
+console.log(alternateName);
+
+// let
+
+
