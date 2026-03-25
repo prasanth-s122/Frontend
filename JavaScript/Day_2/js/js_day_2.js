6f62b9bb7b7495210b593b261b642cc54aef9fc2
prasanth_s <prasanthsakthi122000@gmail.com>
2026-03-25 15:28:05 +0530

    JS Day 2 update

---
+++ JavaScript/Day_2/js/js_day_2.js
@@ -0,0 +1,270 @@
+// Var
+
+// 1
+
+var student_id = 10
+console.log('Initialization - ' + student_id);
+
+var student_id
+console.log('Re-Declaration - ' + student_id);
+
+student_id = 20
+console.log('Assign - ' + student_id);
+
+student_id = 30
+console.log('Re-Assign - ' + student_id);
+
+console.log('END OF THE SET');
+// 2
+
+var teacher_name
+console.log('Declaration - ' + teacher_name);
+
+teacher_name = 'John' 
+console.log('Initialization - ' + teacher_name);
+
+teacher_name = 'Snow'
+console.log('Re-Assign - ' + teacher_name);
+
+var teacher_name
+console.log('Re-Declaration - ' + teacher_name);
+
+console.log('END OF THE SET');
+
+// 3
+
+var librarycard_number
+console.log('Declaration - ' + librarycard_number);
+
+librarycard_number = 1 
+console.log('Initialization - ' + librarycard_number);
+
+librarycard_number = 100
+console.log('Re-Assign - ' + librarycard_number);
+
+var librarycard_number
+console.log('Re-Declaration - ' + librarycard_number);
+
+console.log('END OF THE SET');
+
+// 4
+
+var exam_score
+console.log('Declaration - ' + exam_score);
+
+exam_score = 50 
+console.log('Initialization - ' + exam_score);  
+
+exam_score = 100
+console.log('Re-Assign - ' + exam_score);
+
+var exam_score
+console.log('Re-Declaration - ' + exam_score);
+
+console.log('END OF THE SET');
+
+// 5
+
+var department_name
+console.log('Declaration - ' + department_name);
+
+department_name = 'Computer Science' 
+console.log('Initialization - ' + department_name); 
+
+department_name = 'Information Technology'
+console.log('Re-Assign - ' + department_name);
+
+var department_name
+console.log('Re-Declaration - ' + department_name);
+
+console.log('END OF THE SET');
+
+console.log('CONCLUSION - Var allows declaration, initialization, re-assignment and re-declaration of variables.');
+
+
+// let
+
+// 1
+
+let delivery_address
+console.log('Declaration - ' + delivery_address);
+
+delivery_address = 'Chennai'
+console.log('Initialization - ' + delivery_address);
+
+delivery_address = 'Namakkal'
+console.log('Re-Assign - ' + delivery_address);
+
+console.log('END OF THE SET');
+
+
+// 2
+
+let payment_method
+console.log('Declaration - ' + payment_method);
+
+payment_method = 'Credit Card'
+console.log('Initialization - ' + payment_method);
+
+payment_method = 'Debit Card'
+console.log('Re-Assign - ' + payment_method);   
+
+console.log('END OF THE SET');
+
+// 3
+
+let product_category
+console.log('Declaration - ' + product_category);
+
+product_category = 'Electronics'
+console.log('Initialization - ' + product_category);
+
+product_category = 'Clothing'
+console.log('Re-Assign - ' + product_category);
+
+console.log('END OF THE SET');
+
+// 4
+
+let shipping_cost
+console.log('Declaration - ' + shipping_cost);
+
+shipping_cost = 50
+console.log('Initialization - ₹' + shipping_cost);
+
+shipping_cost = 100
+console.log('Re-Assign - ₹' + shipping_cost);
+
+
+console.log('END OF THE SET');
+
+// 5
+
+let product_rating
+console.log('Declaration - ' + product_rating);
+
+product_rating = 3.5
+console.log('Initialization - ' + product_rating + ' stars');
+
+product_rating = 4.5
+console.log('Re-Assign - ' + product_rating + ' stars');
+
+console.log('END OF THE SET');
+
+console.log('CONCLUSION - Let allows declaration, initialization and re-assignment of variables but does not allow re-declaration.');
+console.log('let gives syntax error when re-declaration');
+
+
+// const
+
+// 1
+
+const company_name = 'Accenture'
+console.log('Initialization - ' + company_name);
+
+console.log('END OF THE SET');
+
+// 2
+
+const office_location = 'Perungalathur'
+console.log('Initialization - ' + office_location);
+
+console.log('END OF THE SET');
+
+// 3
+
+const support_email = 'support@accenture.com'
+console.log('Initialization - ' + support_email);
+
+console.log('END OF THE SET');
+
+// 4
+
+const website_url = 'www.accenture.com'
+console.log('Initialization - ' + website_url);
+
+console.log('END OF THE SET');
+
+// 5
+
+const founding_year = 1989
+console.log('Initialization - ' + founding_year);
+
+console.log('END OF THE SET');
+
+// 6
+
+const ceo_name = 'Julie Sweet'
+console.log('Initialization - ' + ceo_name);    
+
+console.log('END OF THE SET');
+
+// 7
+
+const business_type = 'Consulting'
+console.log('Initialization - ' + business_type);
+
+console.log('END OF THE SET');
+
+// 8
+
+const employee_count = 500000
+console.log('Initialization - ' + employee_count);
+
+console.log('END OF THE SET');
+
+// 9
+const annual_revenue = '₹5000 Crores'
+console.log('Initialization - ' + annual_revenue);
+
+console.log('END OF THE SET');
+
+// 10
+
+const communication_language = 'English'
+console.log('Initialization - ' + communication_language);
+
+console.log('END OF THE SET');
+
+// 11
+
+const time_zone = 'IST'
+console.log('Initialization - ' + time_zone);
+
+console.log('END OF THE SET');
+
+// 12
+
+const password_minlength = 8
+console.log('Initialization - ' + password_minlength + ' characters');
+
+console.log('END OF THE SET');
+
+// 13
+
+const app_version = 'v6'
+console.log('Initialization - ' + app_version);
+
+console.log('END OF THE SET');
+
+// 14
+
+const login_attempts = 10
+console.log('Initialization - ' + login_attempts + ' attempts');    
+
+console.log('END OF THE SET');
+
+// 15
+
+const hike_percentage = 10
+console.log('Initialization - ' + hike_percentage + '%');   
+
+console.log('END OF THE SET');
+
+console.log('CONCLUSION - Const allows declaration and initialization of variables but does not allow re-assignment and re-declaration.');
+console.log('Const gives assignment error when re-assigning');
+console.log('Const gives syntax error when re-declaring');
+
+
+
+
