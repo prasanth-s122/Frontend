d10bb58e72a0dee0e0b982f40eb845171094c95b
prasanth_s <prasanthsakthi122000@gmail.com>
2026-04-29 23:41:53 +0530

    Update

--- React/Day_9/formhandling/src/Pages/Register.jsx
+++ React/Day_9/formhandling/src/Pages/Register.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import banner from '../assets/Rbanner.jpg'
 
 const Register = () => {
   
@@ -26,7 +27,7 @@ const Register = () => {
       localStorage.setItem("User Data" , JSON.stringify(registerData) )
 
       
-      alert ("Sucessfully Registered 😊")
+      alert ("Sucessfully Registered 😊") 
       
 
       setRegisterData({name : "" , email : "" , password : "" , mobile : "" , age : ""})
@@ -40,23 +41,23 @@ const Register = () => {
   return (
     <>
     
-        <div>
+        <div className='h-screen flex justify-center items-center'>
           
-          <div>
+          <div className='w-200 border flex justify-between items-center rounded-lg'>
 
-              <div>
-
+              <div >
+                  <img src={banner} className='w-100 p-3 rounded-3xl' alt="" /> 
               </div>
 
               <div>
-                    <form onSubmit={handleSubmit}>
-
-                          <input type="text"      name='name'       value={registerData.name}       onChange={inputChange} placeholder='Enter the name' />
-                          <input type="email"     name='email'      value={registerData.email}      onChange={inputChange} placeholder='Enter email '/>
-                          <input type="password"  name='password'   value={registerData.password}   onChange={inputChange} placeholder='Set password'/>
-                          <input type="tel"       name='mobile'     value={registerData.mobile}     onChange={inputChange} placeholder='Enter Mobile number'/>
-                          <input type="number"    name='age'        value={registerData.age}        onChange={inputChange} placeholder='Enter your Age' min={18} max={150} />
-                          <input type="submit" value="Register" />
+                    <form onSubmit={handleSubmit} className=' flex flex-col gap-3 p-5'>
+                          <h1 className='text-center font-semibold'>Register</h1>
+                          <input type="text"      name='name'       value={registerData.name}       onChange={inputChange} placeholder='Enter the name'  className='p-3 border rounded-lg ' />
+                          <input type="email"     name='email'      value={registerData.email}      onChange={inputChange} placeholder='Enter email '  className='p-3 border rounded-lg '/>
+                          <input type="password"  name='password'   value={registerData.password}   onChange={inputChange} placeholder='Set password'  className='p-3 border rounded-lg '/>
+                          <input type="tel"       name='mobile'     value={registerData.mobile}     onChange={inputChange} placeholder='Enter Mobile number' className='p-3 border rounded-lg '/>
+                          <input type="number"    name='age'        value={registerData.age}        onChange={inputChange} placeholder='Enter your Age' min={18} max={150} className='p-3 border rounded-lg ' />
+                          <input type="submit" value="Register" className='p-3 border rounded-lg bg-blue-300 hover:bg-blue-600 hover:text-white cursor-pointer '/>
 
                     </form>
               </div>
