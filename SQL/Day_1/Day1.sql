33ea09c6cc582f43714f78a16b9e6c8a46b35042
prasanth_s <prasanthsakthi122000@gmail.com>
2026-05-07 19:57:19 +0530

    SQL

---
+++ SQL/Day_1/Day1.sql
@@ -0,0 +1,13 @@
+CREATE database sampletable;
+use sampletable;
+CREATE TABLE products(
+	
+    user_id int PRIMARY KEY,
+    user_name varchar(20),
+    mobile varchar(10),
+    email varchar(20) unique
+    
+
+);
+show tables
+
